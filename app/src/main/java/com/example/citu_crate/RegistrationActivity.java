34d494ea120581f04abf3ff066d7912c17f78c3c
Ranz Lumayno <ranz1211@gmail.com>
2024-05-24 14:47:06 +0800

    Finished Sign up page design

--- app/src/main/java/com/example/citu_crate/RegistrationActivity.java
+++ app/src/main/java/com/example/citu_crate/RegistrationActivity.java
@@ -1,9 +1,6 @@
 package com.example.citu_crate;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LogIn extends AppCompatActivity {
-    Button signup;
+public class RegistrationActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        signup = (Button) findViewById(R.id.btnSignup);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent(LogIn.this, RegistrationActivity.class);
-                startActivity(intent1);
-            }
         });
     }
 }
