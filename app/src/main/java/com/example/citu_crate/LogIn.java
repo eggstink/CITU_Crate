fb1667ae7361683ed689338fa33a7c77541a8de5
Chaz Zyl G. Llaguno <chazzyl.llaguno@gmail.com>
2024-05-26 00:00:41 +0800

    Working registration & login

--- app/src/main/java/com/example/citu_crate/LogIn.java
+++ app/src/main/java/com/example/citu_crate/LogIn.java
@@ -2,17 +2,28 @@ package com.example.citu_crate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LogIn extends AppCompatActivity {
-    //Button signup;
+    Button signIn;
+    private FirebaseAuth auth;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +35,46 @@ public class LogIn extends AppCompatActivity {
             return insets;
         });
 
-        //signup = (Button) findViewById(R.id.btnSignup);
-//        signup.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent1 = new Intent(LogIn.this, RegistrationActivity.class);
-//                startActivity(intent1);
-//            }
-//        });
+        auth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        signIn = (Button) findViewById(R.id.btnSignin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                if (TextUtils.isEmpty(userEmail)) {
+                    Toast.makeText(LogIn.this, "Enter Student ID", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userPassword)) {
+                    Toast.makeText(LogIn.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (userPassword.length() < 8) {
+                    Toast.makeText(LogIn.this, "Password should be atleast 8 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(userEmail,userPassword)
+                        .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(LogIn.this, MainActivity.class));
+                        }else{
+                            Toast.makeText(LogIn.this, "Error:" + task.getException(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+                }
+        });
     }
     public void signUp(View view){
         startActivity(new Intent(LogIn.this, RegistrationActivity.class));
--- app/src/main/java/com/example/citu_crate/RegistrationActivity.java
+++ app/src/main/java/com/example/citu_crate/RegistrationActivity.java
@@ -2,16 +2,29 @@ package com.example.citu_crate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegistrationActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+
+public class RegistrationActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    EditText name, email, password;
+    Button btnsignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +34,61 @@ public class RegistrationActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(RegistrationActivity.this, LogIn.class));
+            finish();
+        }
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnsignUp = findViewById(R.id.btnSignup);
+
+        btnsignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = name.getText().toString();
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(userName)) {
+                    Toast.makeText(RegistrationActivity.this, "Enter Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userEmail)) {
+                    Toast.makeText(RegistrationActivity.this, "Enter Student ID", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userPassword)) {
+                    Toast.makeText(RegistrationActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (userPassword.length() < 8) {
+                    Toast.makeText(RegistrationActivity.this, "Password should be atleast 8 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                        .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegistrationActivity.this, LogIn.class));
+                                }else{
+                                    Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+
+            }
         });
     }
 
