1c040e2f6426624ddb61060ab919bf0f13543b04
Ranz Lumayno <ranz1211@gmail.com>
2024-05-26 11:26:04 +0800

    added fragment_home.xml(notfinished)

--- app/src/main/java/com/example/citu_crate/MainActivity.java
+++ app/src/main/java/com/example/citu_crate/MainActivity.java
@@ -7,18 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment homeFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        homeFragment = new HomeFragment();
+        loadFragment(homeFragment);
+    }
+
+    private void loadFragment(Fragment homeFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.home_container,homeFragment);
+        transaction.commit();
     }
 }
