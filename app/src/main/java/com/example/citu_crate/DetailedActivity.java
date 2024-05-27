a3ab6924db0e482ac522fe138315d2f6024a7d62
ranz <ranz1211@gmail.com>
2024-05-27 10:24:12 +0800

    19

--- app/src/main/java/com/example/citu_crate/DetailedActivity.java
+++ app/src/main/java/com/example/citu_crate/DetailedActivity.java
@@ -3,19 +3,29 @@ package com.example.citu_crate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.Toolbar;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailedActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -27,6 +37,7 @@ public class DetailedActivity extends AppCompatActivity {
     NewProductsModel newProductsModel = null;
     PoplularProductModel poplularProductModel = null;
     ShowAllModel showAllModel = null;
+    FirebaseAuth auth;
     private FirebaseFirestore firestore;
     ShowAllModel showAllModel = null;
 
@@ -115,5 +126,39 @@ public class DetailedActivity extends AppCompatActivity {
             }
         });
 
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addtoCart();
+            }
+        });
+    }
+
+    private void addtoCart() {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        final HashMap<String,Object> cartMap = new HashMap<>();
+
+        cartMap.put("productName",name.getText().toString());
+        cartMap.put("productPrice",price.getText().toString());
+        cartMap.put("currentTime",saveCurrentTime);
+        cartMap.put("productName",saveCurrentDate);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("User").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(DetailedActivity.this,"Added To A Cart", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
     }
 }
