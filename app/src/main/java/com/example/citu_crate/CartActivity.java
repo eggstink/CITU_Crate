90c667db8d8d007f3272fff3f0662949f19e59f8
Chaz Zyl G. Llaguno <chazzyl.llaguno@gmail.com>
2024-05-27 15:23:08 +0800

    View Cart

--- app/src/main/java/com/example/citu_crate/CartActivity.java
+++ app/src/main/java/com/example/citu_crate/CartActivity.java
@@ -4,17 +4,46 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    RecyclerView recyc;
+    List<MyCartModel> cartModelList;
+    MyCartAdapter cartAdapter;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cart);
 
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+
+        toolbar = findViewById(R.id.my_cart_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyc = findViewById(R.id.cart_rec);
+        recyc.setLayoutManager(new LinearLayoutManager(this));
+        cartModelList = new ArrayList<>();
+        cartAdapter = new MyCartAdapter(this, cartModelList);
+        recyc.setAdapter(cartAdapter);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser())
     }
 }
